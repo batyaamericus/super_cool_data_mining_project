66ccbcb783810c797627b7d67152dcfa515b40ce
Ulla Smirnova <99829469+ullasmrnva@users.noreply.github.com>
2022-02-23 22:21:50 +0200

    DRAFT 1

---
+++ scraping.py
@@ -0,0 +1,37 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+import json
+import re
+
+list_of_urls=[]
+with open('urls.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        list_of_urls.append(row['link'].replace('/'.join(row['link'].split('/')[-2:]),''))
+
+i=0;
+for url in list_of_urls:
+    print(url)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    results = soup.find_all('script', type='text/javascript')
+    lol=0
+    for i in results:
+        if re.search('COMPANY_DATA = ({.*})',i.get_text()):
+            company_data= json.loads(re.search('COMPANY_DATA = ({.*})',i.get_text()).group(1))
+            pattern = 'COMPANY_POSITIONS_DATA = (\[{.*}\])'
+            position_data_raw=re.search(pattern,i.get_text()).group(1)
+            positions_data=json.loads(position_data_raw)
+
+            for index, position in enumerate(positions_data):
+                print(position)
+                print(index+1)
+                print('Position:', position['name'])
+                print('Department:', position['department'])
+                print('Location:', position['location']['name'])
+                print('Employment type:', position['employment_type'])
+                print("About:")
+                for y in position['custom_fields']['details']:
+                    print('\t',y['value'])
+                print('\n\n')

8a11ba8d287156d1d4f09993be306b5ec4f08114
Ulla Smirnova <99829469+ullasmrnva@users.noreply.github.com>
2022-03-02 15:43:07 +0200

    Final changes for CheckPoint1

--- scraping.py
+++ scraping.py
@@ -3,39 +3,56 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import re
+import html
+import finding_websites
 
-list_of_urls=[]
-with open('urls.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        list_of_urls.append(row['link'].replace('/'.join(row['link'].split('/')[-2:]),''))
 
-i=0
+def printing_info(string):
+    decoded_string = html.unescape(string)
+    for item in BeautifulSoup(decoded_string, features="html.parser").findAll(text=True):
+        print(item)
+
+list_of_urls = []
+
+for link in finding_websites.extracted_urls:
+    list_of_urls.append(link.replace('/'.join(link.split('/')[-2:]), ''))
+
 for url in list_of_urls:
-    print(url)
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     results = soup.find_all('script', type='text/javascript')
-    lol=0
     for i in results:
         name_of_element = re.search('COMPANY_DATA = ({.*})', i.get_text())
         if name_of_element:
-            company_data= json.loads(name_of_element.group(1))
+            company_data = json.loads(name_of_element.group(1))
 
             pattern = 'COMPANY_POSITIONS_DATA = (\[{.*}\])'
             position_data_raw = re.search(pattern, i.get_text()).group(1)
             positions_data = json.loads(position_data_raw)
 
+            print('Company Info:')
+            try:
+                print('Company name:', company_data['name'])
+                print('Location:', company_data['location'])
+                print('Website:', company_data['website'])
+                if not company_data['linkedin_integrations_apply_context'] == None:
+                    print('Linked in job application')
+
+            except TypeError as ex:
+                print(ex)
+
+            print('\nOpen positions:\n')
+
             for index, position in enumerate(positions_data):
                 try:
-                    print(index+1)
+                    print(index + 1)
                     print('Position:', position['name'])
                     print('Department:', position['department'])
                     print('Location:', position['location']['name'])
                     print('Employment type:', position['employment_type'])
                     print('About:')
                     for y in position['custom_fields']['details']:
-                        print('\t', y['value'])
+                        printing_info(y['value'])
                     print('\n\n')
                 except TypeError as ex:
                     print(ex)

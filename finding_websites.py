39eaf5b5c2952be544e0bf14e505400d60f63768
Batya Americus <batya.americus@gmail.com>
2022-03-02 14:17:06 +0200

    combined files containing code to find websites and to extract the wesite for the job position

--- finding_websites.py
+++ finding_websites.py
@@ -1,17 +1,16 @@
 import requests
 import json
+import csv
 
-url = "https://google-search3.p.rapidapi.com/api/v1/search/q=data+scientist+site:www.comeet.com/jobs&num=1000"
 
-headers = {
+GOOGLE_API_URL = "https://google-search3.p.rapidapi.com/api/v1/search/q=data+scientist+site:www.comeet.com/jobs&num=100"
+GOOGLE_API_HEADERS = {
     'x-user-agent': "desktop",
     'x-proxy-location': "EU",
     'x-rapidapi-host': "google-search3.p.rapidapi.com",
     'x-rapidapi-key': "1cf3e6b18dmsh45dfd10636a972ap138459jsnedbb0fffe534"
     }
 
-response = requests.get(url, headers=headers)
-
-with open(r'C:\Users\Batra\PycharmProjects\ITC_pycharm\course_project\web_response.json', 'w') as file:
-    json.dump(response.json(), file)
-print(response.text)
+google_response = requests.get(GOOGLE_API_URL, headers=GOOGLE_API_HEADERS)
+reading_google_response = json.loads(google_response.text)
+extracted_urls = [job['link'] for job in reading_google_response['results']]

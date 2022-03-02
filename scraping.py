2931fc0447077f50bf5b23b802d751de1ea562aa
Batya Americus <batya.americus@gmail.com>
2022-03-02 17:57:27 +0200

    editorial changes

--- scraping.py
+++ scraping.py
@@ -12,37 +12,41 @@ def printing_info(string):
     for item in BeautifulSoup(decoded_string, features="html.parser").findAll(text=True):
         print(item)
 
-list_of_urls = []
 
+# importing job position urls from finding_websites.py and splitting them to get the company's main url
+list_of_urls = []
 for link in finding_websites.extracted_urls:
     list_of_urls.append(link.replace('/'.join(link.split('/')[-2:]), ''))
 
+# finding relevant elements in page
 for url in list_of_urls:
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     results = soup.find_all('script', type='text/javascript')
-    for i in results:
-        name_of_element = re.search('COMPANY_DATA = ({.*})', i.get_text())
-        if name_of_element:
-            company_data = json.loads(name_of_element.group(1))
 
-            pattern = 'COMPANY_POSITIONS_DATA = (\[{.*}\])'
-            position_data_raw = re.search(pattern, i.get_text()).group(1)
-            positions_data = json.loads(position_data_raw)
+    for i in results:
+        # extracting element with company data
+        company_data_element = re.search('COMPANY_DATA = ({.*})', i.get_text())
+        # extracting element with position data
+        positions_data_element = re.search('COMPANY_POSITIONS_DATA = (\[{.*}\])', i.get_text())
+
+        if company_data_element:
+            company_data = json.loads(company_data_element.group(1))
+            positions_data = json.loads(positions_data_element.group(1))
 
             print('Company Info:')
+            # extracting company data
             try:
                 print('Company name:', company_data['name'])
                 print('Location:', company_data['location'])
                 print('Website:', company_data['website'])
-                if not company_data['linkedin_integrations_apply_context'] == None:
-                    print('Linked in job application')
-
+                if not company_data['linkedin_integrations_apply_context']:
+                    print('Apply via LinkedIn')
             except TypeError as ex:
-                print(ex)
+                pass
 
             print('\nOpen positions:\n')
-
+            # extracting data about positions
             for index, position in enumerate(positions_data):
                 try:
                     print(index + 1)
@@ -55,4 +59,4 @@ for url in list_of_urls:
                         printing_info(y['value'])
                     print('\n\n')
                 except TypeError as ex:
-                    print(ex)
+                    pass

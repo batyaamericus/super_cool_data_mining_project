6b223dbcf1c19c54d3dab5794f45b4f1d38d6bf2
Batya Americus <batya.americus@gmail.com>
2022-03-11 21:46:30 +0200

    no message

--- database_for_comeet_jobs.py
+++ database_for_comeet_jobs.py
@@ -6,14 +6,14 @@ creating a database into which we will put all of the information about the jobs
 
 
 # creating the database "comeet_jobs"
-# mysql_connection = pymysql.connect(host='localhost',
-#                                    user='root',
-#                                    password='abcd',
-#                                    cursorclass=pymysql.cursors.DictCursor)
-# with mysql_connection as mysql_connection:
-#     with mysql_connection.cursor() as mysql_cursor:
-#         sql_command = 'CREATE DATABASE comeet_jobs;'
-#         mysql_cursor.execute(sql_command)
+mysql_connection = pymysql.connect(host='localhost',
+                                   user='root',
+                                   password='abcd',
+                                   cursorclass=pymysql.cursors.DictCursor)
+with mysql_connection as mysql_connection:
+    with mysql_connection.cursor() as mysql_cursor:
+        sql_command = 'CREATE DATABASE comeet_jobs;'
+        mysql_cursor.execute(sql_command)
 
 
 # connecting to the comeet_jobs database in order to fill it with the relevant tables
@@ -25,33 +25,33 @@ mysql_connection = pymysql.connect(host='localhost',
 with mysql_connection as mysql_connection:
     with mysql_connection.cursor() as mysql_cursor:
         # company tables
-        # companies_table = 'CREATE TABLE companies ( \
-        #                                        company_uid VARCHAR(6) PRIMARY KEY, \
-        #                                        company_name VARCHAR(256) NOT NULL, \
-        #                                        location VARCHAR(256), \
-        #                                        website VARCHAR(256) \
-        #                                        );'
-        # mysql_cursor.execute(companies_table)
-        # company_description = 'CREATE TABLE company_description ( \
-        #                                        company_uid VARCHAR(6), \
-        #                                        description LONGTEXT, \
-        #                                        FOREIGN KEY (company_uid) REFERENCES companies(company_uid) \
-        #                                        );'
-        # mysql_cursor.execute(company_description)
-        #
-        # # position tables
-        # positions_table = 'CREATE TABLE positions ( \
-        #                                      position_uid VARCHAR(6) PRIMARY KEY, \
-        #                                      position_name VARCHAR(256) NOT NULL, \
-        #                                      department VARCHAR(256), \
-        #                                      location VARCHAR(256), \
-        #                                      employment_type VARCHAR(256), \
-        #                                      experience_level VARCHAR(50), \
-        #                                      time_updated DATETIME, \
-        #                                      company_uid VARCHAR(6), \
-        #                                      FOREIGN KEY (company_uid) REFERENCES companies(company_uid) \
-        #                                      );'
-        # mysql_cursor.execute(positions_table)
+        companies_table = 'CREATE TABLE companies ( \
+                                               company_uid VARCHAR(6) PRIMARY KEY, \
+                                               company_name VARCHAR(256) NOT NULL, \
+                                               location VARCHAR(256), \
+                                               website VARCHAR(256) \
+                                               );'
+        mysql_cursor.execute(companies_table)
+        company_description = 'CREATE TABLE company_description ( \
+                                               company_uid VARCHAR(6), \
+                                               description LONGTEXT, \
+                                               FOREIGN KEY (company_uid) REFERENCES companies(company_uid) \
+                                               );'
+        mysql_cursor.execute(company_description)
+
+        # position tables
+        positions_table = 'CREATE TABLE positions ( \
+                                             position_uid VARCHAR(6) PRIMARY KEY, \
+                                             position_name VARCHAR(256) NOT NULL, \
+                                             department VARCHAR(256), \
+                                             location VARCHAR(256), \
+                                             employment_type VARCHAR(256), \
+                                             experience_level VARCHAR(50), \
+                                             time_updated DATETIME, \
+                                             company_uid VARCHAR(6), \
+                                             FOREIGN KEY (company_uid) REFERENCES companies(company_uid) \
+                                             );'
+        mysql_cursor.execute(positions_table)
         positions_description = 'CREATE TABLE positions_description ( \
                                                  position_uid VARCHAR(6), \
                                                  description LONGTEXT, \
